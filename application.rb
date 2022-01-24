78d2a2061ade85622ae3ee0e26b76f2436eed85e
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-01-24 14:38:00 -0500

    Added "full board" game over condition

--- application.rb
+++ application.rb
@@ -1,33 +1,28 @@
+# frozen_string_literal: true
+
 # models a Tic Tac Toe game
 class Game
-  def initialize(player_1, player_2)
+  def initialize(player1, player2)
     @board = %w[
       a b c
       d e f
       g h i
     ]
 
-    @players = [player_1, player_2]
+    @players = [player1, player2]
 
     puts 'Player 1 goes first'
-    self.play
+    play
   end
 
   def play
     turn = 0
-
     loop do
       display_board
-      if turn.even?
-        current_player = @players[0]
-      else
-        current_player = @players[1]
-      end
-      print "\n#{current_player.name}: "
-      choice = gets.chomp
-      if @board.include?(choice)
-        @board[@board.index(choice)] = current_player.marker
-      elsif choice == 'q'
+      current_player = select_player(turn)
+      get_choice(current_player)
+      if board_full?(turn)
+        puts 'Game over.'
         break
       end
       turn += 1
@@ -43,11 +38,45 @@ class Game
     puts '----------'
     puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
   end
+
+  private
+
+  def vaild_position?(choice)
+    @board.include?(choice) ? true : false
+  end
+
+  def select_player(turn)
+    if turn.even?
+      @players[0]
+    else
+      @players[1]
+    end
+  end
+
+  def get_choice(player)
+    print "\n#{player.name}: "
+    choice = gets.chomp
+    if vaild_position?(choice)
+      @board[@board.index(choice)] = player.marker
+    else
+      invalid_choice(player)
+    end
+  end
+
+  def invalid_choice(player)
+    puts 'Selection invalid.'
+    display_board
+    get_choice(player)
+  end
+
+  def board_full?(turn)
+    true if turn >= 8
+  end
 end
 
+# Models player attributes for Tic Tac Toe game
 class Player
-  attr_reader :marker
-  attr_reader :name
+  attr_reader :marker, :name
 
   def initialize(name, marker)
     @name = name
@@ -55,4 +84,4 @@ class Player
   end
 end
 
-new_game = Game.new(Player.new('Player 1', 'X'), Player.new('Player 2', 'O'))
+Game.new(Player.new('Player 1', 'X'), Player.new('Player 2', 'O'))
