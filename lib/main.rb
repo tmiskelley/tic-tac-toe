c67051b030609a23149ab520ab15faae5f07c924
Timothy Miskelley <miskelleytimothy@gmail.com>
2022-11-16 12:57:09 -0500

    Created tests for player win conditions

--- lib/main.rb
+++ lib/main.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-# frozen_string_literal: true
+require './lib/tic_tac_toe'
 
 TicTacToe.new(Player.new('Player 1', 'X'), Player.new('Player 2', 'O')).play
--- spec/test_tic_tac_toe.rb
+++ spec/test_tic_tac_toe.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-require './lib/application'
+require './lib/tic_tac_toe'
 
 describe TicTacToe do
   describe '#select_player' do
@@ -24,6 +24,31 @@ describe TicTacToe do
       it 'switches to player 1' do
         current_player = tictactoe.send(:select_player)
         expect(current_player).to eq(player1)
+      end
+    end
+  end
+
+  describe '#player_win?' do
+    let(:player1) { double('player1', marker: 'X') }
+    let(:player2) { double('player2') }
+    subject(:tictactoe) { described_class.new(player1, player2) }
+
+    context 'when the current lines do not match a win condition' do
+      it 'returns false' do
+        result = tictactoe.send(:player_win?, player1)
+        expect(result).to be(false)
+      end
+    end
+
+    context 'When the current lines match a win condition' do
+      before do
+        win_array = [nil, 'X', 'O', 'O', nil, 'X', nil, 'O', nil, 'X', nil]
+        tictactoe.instance_variable_set(:@board, win_array)
+      end
+
+      it 'returns true' do
+        result = tictactoe.send(:player_win?, player1)
+        expect(result).to be(true)
       end
     end
   end
